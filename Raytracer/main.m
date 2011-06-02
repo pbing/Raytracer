3ea760e45a1883234eef5b2774e971efa31adb86
Bernd Beuster <bernd@flexo.fritz.box>
2011-06-02 11:54:23 +0200

    Initial Commit

---
+++ Raytracer/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Raytracer
+//
+//  Created by Bernd Beuster on 02.06.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    // insert code here...
+    NSLog(@"Hello, World!");
+
+    [pool drain];
+    return 0;
+}
+
